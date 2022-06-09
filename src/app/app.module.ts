29c3e0de50dc9200bbaa4d9b29b9bed9205722a0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-09 22:51:13 +0530

    Add page-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    PageListComponent
   ],
   imports: [
     BrowserModule,
