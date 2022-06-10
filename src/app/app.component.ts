9d1c94c32a9609e64c62dc30d7bb30143416f287
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-10 23:29:25 +0530

    Call search method, subscribe to it and console log result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent {
   }
 
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    console.log(results);
+    this.wikipedia.search(term).subscribe((response) => {
+      console.log(response)
+    });
   }
 }
