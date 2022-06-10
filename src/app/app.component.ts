43f11c27f7aee1e8e80c5ba146015a10fa413eb8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-10 23:33:49 +0530

    Add pages array and assign pages to response.query.search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,18 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
   title = 'explore';
+  
+  pages = [
+
+  ];
 
   constructor(private wikipedia: WikipediaService) {
 
   }
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response) => {
-      console.log(response)
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
     });
   }
 }
